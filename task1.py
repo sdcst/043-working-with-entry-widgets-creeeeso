a568c12c5bb0e82fcc1378120fc94cd9d9f3ea70
creeeeso <crystalf7319@gmail.com>
2024-06-05 12:20:40 -0700

    1

--- task1.py
+++ task1.py
@@ -7,3 +7,23 @@ Create entry widets to allow user to enter their:
 
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
+import tkinter as tk
+import playsound as p
+win = tk.Tk()
+
+def name(e):
+    print("Enter your name: ")
+    print(f"Your name is{e}")
+    data = e1.get()
+    l1.config(text=data)
+    e2.delete(0,tk.END)
+    e2.insert(0,data)
+
+l1 = tk.Label(win, text="N/A")
+e1 = tk.Entry(win, width=15)
+l2 = tk.Label(win, text="Enter your student number: ")
+e2 = tk.Entry(win, width=15)
+l3 = tk.Label(win, text="Enter your grade: ")
+e3 = tk.Entry(win, width=15)
+
+e1.pack()
