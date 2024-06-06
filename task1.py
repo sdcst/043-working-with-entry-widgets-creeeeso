3ce5ba50a3265b94047c3da1a6a2b49975ff4753
creeeeso <crystalf7319@gmail.com>
2024-06-06 12:30:19 -0700

    1

--- task1.py
+++ task1.py
@@ -9,21 +9,31 @@ Create a button so that when they click on the button, it states all of the info
 """
 import tkinter as tk
 import playsound as p
-win = tk.Tk()
 
 def name(e):
-    print("Enter your name: ")
-    print(f"Your name is{e}")
-    data = e1.get()
+    data1 = e1.get()
+    data2 = e2.get()
+    data3 = e3.get()
+    data = "Name: " + data1 +"\n " + "Student Number: " + data2 + "\n" + "Grade: " + data3
+
     l1.config(text=data)
     e2.delete(0,tk.END)
     e2.insert(0,data)
+    e3.delete(0,tk.END)
+    e3.insert(0,data)
 
-l1 = tk.Label(win, text="N/A")
+win = tk.Tk()
+
+b1 = tk.Button(win, text="Click to change the text")
+l1 = tk.Label(win, text="Enter your student number:")
 e1 = tk.Entry(win, width=15)
 l2 = tk.Label(win, text="Enter your student number: ")
 e2 = tk.Entry(win, width=15)
 l3 = tk.Label(win, text="Enter your grade: ")
 e3 = tk.Entry(win, width=15)
 
-e1.pack()
+l1.grid(row=1, column=1)
+e1.grid(row=2, column=1)
+
+
+win.mainloop()
