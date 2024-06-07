68fdb0798b5fe8595d6af51cd036783f82f4c848
creeeeso <crystalf7319@gmail.com>
2024-06-07 12:48:29 -0700

    2

--- task1.py
+++ task1.py
@@ -8,32 +8,37 @@ Create entry widets to allow user to enter their:
 Create a button so that when they click on the button, it states all of the information in a 4th entry widget
 """
 import tkinter as tk
-import playsound as p
 
-def name(e):
+def run(e):
     data1 = e1.get()
     data2 = e2.get()
     data3 = e3.get()
     data = "Name: " + data1 +"\n " + "Student Number: " + data2 + "\n" + "Grade: " + data3
-
-    l1.config(text=data)
-    e2.delete(0,tk.END)
-    e2.insert(0,data)
-    e3.delete(0,tk.END)
-    e3.insert(0,data)
+    l4.config(text=data)
+    e4.delete(0,tk.END)
+    e4.insert(0,data)
 
 win = tk.Tk()
 
-b1 = tk.Button(win, text="Click to change the text")
-l1 = tk.Label(win, text="Enter your student number:")
+b1 = tk.Button(win, text="Click to save information")
+l1 = tk.Label(win, text="Enter your name: ")
 e1 = tk.Entry(win, width=15)
 l2 = tk.Label(win, text="Enter your student number: ")
 e2 = tk.Entry(win, width=15)
 l3 = tk.Label(win, text="Enter your grade: ")
 e3 = tk.Entry(win, width=15)
+l4 = tk.Label(win, text="N/A")
+e4 = tk.Entry(win, width=15)
+
+b1.bind("<Button-1>",run)
 
 l1.grid(row=1, column=1)
 e1.grid(row=2, column=1)
-
+l2.grid(row=1, column=2)
+e2.grid(row=2, column=2)
+l3.grid(row=1, column=3)
+e3.grid(row=2, column=3)
+b1.grid(row=3, column=2)
+l4.grid(row=4, column=2)
 
 win.mainloop()
--- task2.py
+++ task2.py
@@ -6,3 +6,35 @@ The first 2 entry widgets allow the user to enter in the 2 short sides of a righ
 When the button is clicked, calculate the length of the hypotenuse and display it in the 3rd entry widget.
 Any labels you need for instruction are optional.
 """
+import tkinter as tk
+
+win = tk.Tk()
+
+def clickFunction(e):
+    data1 = e1.get()
+    data2 = e2.get()
+    number1 = float(data1)
+    number2 = float(data2)
+    data = ((number1**2) + (number2**2)) ** 0.5
+    e3.insert(0,data)
+
+b1 = tk.Button(win, text="Click to find the hypotenuse")
+l10 = tk.Label(win, text="There are 3 side in a triangle, 2 short side (typically called side a and b)\nand one long side (the hypotenuse)")
+l1 = tk.Label(win, text="Enter side a: ")
+e1 = tk.Entry(win, width=15)
+l2 = tk.Label(win, text="Enter side b:")
+e2 = tk.Entry(win, width=15)
+l3 = tk.Label(win, text="N/A")
+e3 = tk.Entry(win, width=15)
+
+b1.bind("<Button-1>", clickFunction)
+
+l1.grid(row=1, column=1)
+e1.grid(row=1, column=2)
+l2.grid(row=2, column=1)
+e2.grid(row=2, column=2)
+l3.grid(row=3, column=1)
+b1.grid(row=4, column=2)
+e3.grid(row=3, column=2)
+
+win.mainloop()
